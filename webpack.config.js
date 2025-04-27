04ef0e1fe9c9b3b959e14c869cc23e9564f44333
creativival <creativival@gmail.com>
2025-04-27 09:50:09 +0900

    voxelamming, voxelammingTurtleのエクステンションを開発できる

--- webpack.config.js
+++ webpack.config.js
@@ -195,18 +195,18 @@ const buildConfig = baseConfig.clone()
         ]
     }));
 
-// Enable HTTPS for the dev server
-buildConfig.merge({
-    devServer: {
-        server: {
-            type: 'https',
-            options: {
-                key: '.vscode/localhost-key.pem',
-                cert: '.vscode/localhost.pem'
-            }
-        }
-    }
-});
+// // Enable HTTPS for the dev server
+// buildConfig.merge({
+//     devServer: {
+//         server: {
+//             type: 'https',
+//             options: {
+//                 key: '.vscode/localhost-key.pem',
+//                 cert: '.vscode/localhost.pem'
+//             }
+//         }
+//     }
+// });
 
 // resolve symlinks for local development
 baseConfig.merge({
